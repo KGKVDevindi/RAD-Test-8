747405ed2c7d194c77f59a03ea7db992b4f8d04b
ADMIN <ADMIN@VISH>
2024-01-28 23:08:33 +0530

    Add project files.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "John";
+            Console.WriteLine(name);
+            int myNum = 15;
+            Console.WriteLine(myNum);
+        }
+    }
+}
